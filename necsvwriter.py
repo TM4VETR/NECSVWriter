d6e4fc81c62dc9ebfaed840e8ca323b9e9896456
rfechner <richard.fechner.nr@gmail.com>
2022-12-02 11:30:48 +0100

    Added custom typesystem

--- necsvwriter.py
+++ necsvwriter.py
@@ -4,26 +4,32 @@ import logging
 from cassis import *
 import csv
 
+# create a option parser instance, allowing us to specify custom options. We create a option '-o --output' to enable
+# the user to choose their preferred output filename.
 parser = OptionParser()
 parser.add_option("-o", "--output", dest="filename",default="", action="store", type="string",
                   help="The putput file", metavar="filename")
-args = parser.parse_args()
 
+# read the options from stdin
 (options, args) = parser.parse_args()
 
-c = ""
+# accumulate the xmi from stdin into a string
+xmi_representation = ""
 for line in sys.stdin:
-    c = c + line
+    xmi_representation += line
 
-# Default Typesystem
-typesystem=load_dkpro_core_typesystem()
-# make cas out of stdin
-cas = load_cas_from_xmi(c, typesystem=load_dkpro_core_typesystem())
+# create CAS from xmi and typesystem
+with open('sample_typesystem.xml', 'rb') as f:
+    sample_typesystem = load_typesystem(f)
 
-with open(options.filename, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=';',
+merged_typesystem = merge_typesystems(sample_typesystem, load_dkpro_core_typesystem())
+cas = load_cas_from_xmi(xmi_representation, typesystem=merged_typesystem)
+
+# write tokens of CAS into a csv file.
+with open(options.filename, 'w', newline='\n') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for token in cas.select('de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity'):
-        writer.writerow ([cas.sofa_string[int(token.begin):int(token.end)], token.begin, token.end, token.value])
+        writer.writerow([cas.sofa_string[int(token.begin):int(token.end)], token.begin, token.end, token.value])
 
 
